309cc103f3b1fc8af45456143f1247abc0e553b1
Yueyue Sun <112975572+zppqako@users.noreply.github.com>
2023-09-28 20:00:19 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,89 @@
-from CTkScrollableDropdown import *
 import customtkinter
-
+import sqlite3
+import hashlib
+import bcrypt
+from tkinter import*
+from tkinter import messagebox
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
 
-root.geometry("700x700")
+root.geometry("700x400")
 
-username = [] * 10
-password = [] * 10
+# conn = sqlite3.connect('data.db')
+# cursor = conn.cursor()
+#
+# cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS users (
+#         username TEXT NOT NULL,
+#         password TEXT NOT NULL)''')
+users = {}
 
-def login():
+# def register():
+#     username = entry1.get()
+#     password = entry2.get()
+#     if username != '' and password != '':
+#         cursor.execute('SELECT username FROM users WHERE username=?', [username])
+#         if cursor.fetchone() is not None:
+#             messagebox.showerror('Error', 'Username already exists.')
+#         else:
+#             encoded_password = password.encode('utf-8')
+#             hashed_password = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
+#             cursor.execute('INSERT INTO users VALUES (?, ?)', [username,hashed_password])
+#             conn.commit()
+#             messagebox.showinfo('Success', 'Account has been created.')
+#     elif username == '' and password == '':
+#         messagebox.showerror('Error', 'Enter all data.')
 
-    if username[0] == entry1.get():
-        if password[0] == entry2.get():
-            print("next page")
-        else:
-            print("username or password is not correct")
+
+def save_users():
+    with open("users.txt", 'w') as file:
+        for username, password in users.items():
+            file.write(f"{username}:{password}\n")
+
+def load_users():
+    try:
+        with open("users.txt", 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                username, password = line.strip().split(":")
+                users[username] = password
+    except FileNotFoundError:
+        pass
 
 def register():
-    username.append(entry1.get())
-    password.append(entry2.get())
-    print (username[0])
-    print (password[0])
+    username = entry1.get()
+    password = entry2.get()
+    if len(username) == 0 or len(password) == 0:
+        messagebox.showerror('Error', 'Enter all data.')
+        return
+    if username in users:
+        messagebox.showerror('Error', 'Username already exists.')
+        return
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    users[username] = hashed_password
+    save_users()
+    messagebox.showinfo('Success', 'Account has been created.')
+    entry1.delete(0, 'end')
+    entry2.delete(0, 'end')
+
+def login():
+    username = entry1.get()
+    password = entry2.get()
+    if len(username) == 0 or len(password) == 0:
+        messagebox.showerror('Error', 'Enter all data.')
+        return
+    if username not in users:
+        messagebox.showerror('Error', 'User does not exist.')
+        return
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    if users[username] == hashed_password:
+        messagebox.showinfo('Success', 'Login successfully')
+        print("1")
+    else:
+        messagebox.showerror('Error', 'Password is not correct.')
 
 
 
@@ -40,45 +99,12 @@ entry1.pack(pady=12, padx=10)
 entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
 entry2.pack(pady=12, padx=10)
 
-button1 = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button1 = customtkinter.CTkButton(master=frame, command = login, text="Login", cursor = 'hand2')
 button1.pack(pady=12, padx=10)
 
-button2 = customtkinter.CTkButton(master=frame, text="Register here", command=register)
+button2 = customtkinter.CTkButton(master=frame, command = register, text="Register here", cursor = 'hand2')
 button2.pack(pady=12, padx=10)
 
 
-customtkinter.CTkLabel(root, text="Different Dropdown Styles").pack(pady=5)
-
-# Some option list
-values = ["python","tkinter","customtkinter","widgets",
-          "options","menu","combobox","dropdown","search"]
-
-# Attach to OptionMenu
-optionmenu = customtkinter.CTkOptionMenu(root, width=240)
-optionmenu.pack(fill="x", padx=10, pady=10)
-
-CTkScrollableDropdown(optionmenu, values=values)
-
-# Attach to Combobox
-combobox = customtkinter.CTkComboBox(root, width=240)
-combobox.pack(fill="x", padx=10, pady=10)
-
-CTkScrollableDropdown(combobox, values=values, justify="left", button_color="transparent")
-
-# Attach to Entry
-customtkinter.CTkLabel(root, text="Live Search Values").pack()
-
-entry = customtkinter.CTkEntry(root, width=240)
-entry.pack(fill="x", padx=10, pady=10)
-
-CTkScrollableDropdown(entry, values=values, command=lambda e: entry.insert(1, e),
-                      autocomplete=True) # Using autocomplete
-
-# Attach to Button
-button = customtkinter.CTkButton(root, text="choose options", width=240)
-button.pack(fill="x", padx=10, pady=10)
-
-CTkScrollableDropdown(button, values=values, height=270, resize=False, button_height=30,
-                      scrollbar=False, command=lambda e: button.configure(text=e))
 
 root.mainloop()
